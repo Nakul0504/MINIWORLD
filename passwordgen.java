e0f255a37686cb9e1be233d88a27a3b51b579f01
Nakul0504 <117922617+Nakul0504@users.noreply.github.com>
2023-07-26 18:58:19 +0530

    passwordgen.java

--- passwordgen.java
+++ passwordgen.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 import java.util.Random;
 class passwordgen {
     public static void main(String[] args) {
+	    //initialize the required things
         String capalpha="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 		String smallalpha="abcdefghijklmnopqrstuvwxyz";
 		String nums="1234567890";
@@ -13,17 +14,17 @@ class passwordgen {
 		int count=0;
 		
 		
-		
+		//instruction for the user
 		System.out.println("Welcome to Online Password Generator ...\n");
 		System.out.println("PRESS yes OR no FOR THE FOLLOWING QUESTIONS..\n");
 		
 		
-		System.out.println("1)Do you like to add CAPITAL APLHABETS:");
+		System.out.println("1)Do you like to add CAPITAL APLHABETS:");		//asking for selection for capital letter
 		String select=sr.nextLine();
-		if(select.equals("yes")){
+		if(select.equals("yes")){		//if equal chnage the state for selected and concat to the common string
 			iscap=true;
 			result=result.concat(capalpha);
-			count++;
+			count++;			//counting the no of selections selected
 		}
 		System.out.println("2)Do you like to add SMALL APLHABETS:");
 		select=sr.nextLine();
@@ -46,14 +47,14 @@ class passwordgen {
 			result=result.concat(symbols);
 			count++;
 		}
-		System.out.println("Enter your desired password length :\n");
+		System.out.println("Enter your desired password length :\n");		//input for required length
 		int length=Integer.parseInt(sr.nextLine());
 		if(length<1 || length<count){
-		    throw new ArithmeticException("Please enter a valid length for processing... ");
+		    throw new ArithmeticException("Please enter a valid length for processing... ");		//throws exception for valid length
 		}
 		char output[]=new char[length];
 		int i=0;
-	     if(iscap){
+	     if(iscap){						//if capitalletter is selected ,added to the output via random
 		        output[i]=capalpha.charAt(random.nextInt(capalpha.length()));
 		        i++;
 		    }
@@ -74,6 +75,6 @@ class passwordgen {
 		    output[j]=result.charAt(random.nextInt(result.length()));
 		}
 		System.out.println("The Generated Password is :");
-		System.out.println(output);
+		System.out.println(output);				//displaying the output
     }
 }
